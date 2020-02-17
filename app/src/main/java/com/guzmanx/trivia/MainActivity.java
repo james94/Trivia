5b8fed0802c8a38c337128e63b8fbb94f8a1f33a
james94 <11840449+james94@users.noreply.github.com>
2020-02-16 16:12:33 -0800

    Added Code to getQuestions from QuestionBank
    Added code to get questions from the QuestionBank
    using QuestionBank class. Inside getQuestions()
    method, the JSONArray response data is passed to
    debug log. Verified that the json data does show
    up in logcat

--- app/src/main/java/com/guzmanx/trivia/MainActivity.java
+++ app/src/main/java/com/guzmanx/trivia/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.guzmanx.trivia.controller.AppController;
+import com.guzmanx.trivia.data.QuestionBank;
+
+// https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new QuestionBank().getQuestions();
     }
 }
