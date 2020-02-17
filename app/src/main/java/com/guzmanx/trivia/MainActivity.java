53e92a32ef30e232389d3a839474c232965d6468
james94 <11840449+james94@users.noreply.github.com>
2020-02-16 21:35:05 -0800

    Added next and prev button functionality
    Added functionality for next and prev button. So, user can
    go to next question when he or she clicks the next button.
    Similarly, user can go to previous question when he or she
    clicks the previous button. The previous button will stop
    if user is on the first question.

--- app/src/main/java/com/guzmanx/trivia/MainActivity.java
+++ app/src/main/java/com/guzmanx/trivia/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.guzmanx.trivia.controller.AppController;
 import com.guzmanx.trivia.data.AnswerListAsyncResponse;
@@ -15,19 +19,65 @@ import java.util.List;
 
 // https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView questionTextview;
+    private TextView questionCounterTextview;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private int currentQuestionIndex = 0;
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Question> questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+        nextButton = findViewById(R.id.next_button);
+        prevButton = findViewById(R.id.prev_button);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        questionCounterTextview = findViewById(R.id.counter_text);
+        questionTextview = findViewById(R.id.question_textview);
+
+        nextButton.setOnClickListener(this);
+        prevButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
             @Override
             public void processFinished(ArrayList<Question> questionArrayList) {
+                questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
                 Log.d("Inside", "processFinished: " + questionArrayList);
             }
         });
         //Log.d("Main", "onCreate: " + questionList);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.prev_button:
+                if(currentQuestionIndex > 0) {
+                    currentQuestionIndex = (currentQuestionIndex - 1) % questionList.size();
+                    updateQuestion();
+                }
+                break;
+            case R.id.next_button:
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+                updateQuestion();
+                break;
+            case R.id.true_button:
+                break;
+            case R.id.false_button:
+                break;
+        }
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        questionTextview.setText(question);
     }
 }
