91a05b5b0f19569703d8db13b6d7d149f6651be8
james94 <11840449+james94@users.noreply.github.com>
2020-02-16 18:29:49 -0800

    Passd a new instance of Response interface to QuestionBank getQuestions()
    Inside MainActivity class, onCreate() method, I created
    questionList, assigned it new QuestionBank().getQuestions()
    , which should return a List of all Questions. Inside
    getQuestions(...), I passed a new AnswerListAsyncResponse()
    followed by implementation of processFinished(...) to get
    the questionArrayList, then pass this list to debug log.
    The result was I was able to see all the questions from the list

--- app/src/main/java/com/guzmanx/trivia/MainActivity.java
+++ app/src/main/java/com/guzmanx/trivia/MainActivity.java
@@ -3,9 +3,15 @@ package com.guzmanx.trivia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.guzmanx.trivia.controller.AppController;
+import com.guzmanx.trivia.data.AnswerListAsyncResponse;
 import com.guzmanx.trivia.data.QuestionBank;
+import com.guzmanx.trivia.model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // https://raw.githubusercontent.com/curiousily/simple-quiz/master/script/statements-data.json
 
@@ -16,6 +22,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new QuestionBank().getQuestions();
+        List<Question> questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+            @Override
+            public void processFinished(ArrayList<Question> questionArrayList) {
+                Log.d("Inside", "processFinished: " + questionArrayList);
+            }
+        });
+        //Log.d("Main", "onCreate: " + questionList);
     }
 }
